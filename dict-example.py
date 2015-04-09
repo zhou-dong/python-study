270229675927254dbb02f4972d292e62f47ba95b
zhou-dong <82224165@qq.com>
2015-04-09 13:40:16 -0500

    update dict

---
+++ dict-example.py
@@ -0,0 +1,34 @@
+dictionary = {"apple": 1, "banana": 10, "melon": 4}
+
+print dictionary
+
+print dir(dictionary)
+
+print dictionary.get("car")
+
+print dictionary.get("car", 0)
+
+dictionary["car"] = 2
+
+print dictionary
+
+for item in dictionary:
+    print item
+
+# Which means by default, dict in python execyte its keys
+if "melon" in dictionary:
+    print "yes"
+else:
+    print "nope"
+
+print dictionary.keys()
+
+print dictionary.values()
+
+print dictionary.items()
+
+print dictionary
+
+# This is amazing method, I become really like python
+for k,v in dictionary.items():
+    print k, v
