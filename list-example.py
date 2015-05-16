7580599c0c62e4370a97f23d0e6e4d031475d3c7
zhou-dong <82224165@qq.com>
2015-05-15 21:13:26 -0500

    add zip

--- list-example.py
+++ list-example.py
@@ -49,3 +49,16 @@ print 'Your choices are:'
 for index, item in enumerate(choices):
     print index+1, item
 
+'''
+It's also common to need to iterate over two lists at once. This is where the built-in zip function comes in handy.
+
+zip will create pairs of elements when passed two lists, and will stop at the end of the shorter list.
+
+zip can handle three or more lists as well!
+'''
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+for a, b in zip(list_a, list_b):
+    # Add your code here!
+    print max(a,b)
