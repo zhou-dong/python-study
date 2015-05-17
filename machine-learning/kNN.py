5e84d4f035770655f31681ff6493a3a766eb7680
zhou-dong <82224165@qq.com>
2015-05-17 16:16:04 -0500

    add comment of knn

--- machine-learning/kNN.py
+++ machine-learning/kNN.py
@@ -1,2 +1,15 @@
+'''
+KNN: k nearest neighbour
+    It is a supervised learning method
+Step:
+    1. calculate distance with all the data
+    2. find the k nearest data
+    3. from the shortest distance find the result
+advantage:
+    simple
+week:
+    1. must calculte all the distance of the data
+    2. only can categorize, can not predict percent
+'''
 
-Prepared data
+
