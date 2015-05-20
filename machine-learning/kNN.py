2f794943919b2a0208cf10467a2796e951fc0a84
zhou-dong <82224165@qq.com>
2015-05-19 23:51:09 -0500

    update knn

--- machine-learning/kNN.py
+++ machine-learning/kNN.py
@@ -19,3 +19,9 @@ def createDataset():
     group = array([[1.0, 1.1],[1.0, 1.0],[0, 0],[0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
+
+group, labels = createDataset()
+
+print group
+
+print labels
