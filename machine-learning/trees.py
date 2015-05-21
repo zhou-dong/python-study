c4ec6c890bbdc1d30b0cbdd72d2ca91a43716d70
zhou-dong <82224165@qq.com>
2015-05-21 00:36:16 -0500

    add comment of trees

---
+++ machine-learning/trees.py
@@ -0,0 +1,10 @@
+'''
+decision tree
+
+1. find the most efficient category to sepeare the data
+    - infromation theory
+    - greedy algorithm
+
+2. repeat the first step(recursive), until all the end
+
+'''
