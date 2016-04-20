203a6a803f1df154c47a86e1b769ddffc55663fd
zhou-dong <82224165@qq.com>
2016-04-20 12:01:32 -0500

    update

--- computer-vision/contours.py
+++ computer-vision/contours.py
@@ -2,3 +2,15 @@ import numpy as np
 import cv2
 
 path = "/Users/dongdong/Desktop/DSC_1298.JPG"
+
+im = cv2.imread(path)
+
+print im.shape #check if the image is loaded correctly
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,127,255,0)
+contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(im,contours,-1,(0,255,0),3)
+
+cv2.imshow("window title", im)
+cv2.waitKey()
+
