5392947909ab7f4277141fc4f32a6ccf071b5e29
zhou-dong <82224165@qq.com>
2016-04-21 02:44:48 -0500

    update

--- computer-vision/distance.py
+++ computer-vision/distance.py
@@ -26,15 +26,34 @@ def rightMost(line):
 
 def iteratorLine():
     global im
+    minimum = 100000000 
+    maximum = 0
+    temp = 0
+    minimumIndex = 0
+    maximumIndex = 0
     for index, line in enumerate(im):
         margin = rightMost(line) - leftMost(line)
         drawLine(line, margin, index)
-        print margin
+        if(index > 200 and index< 500 and margin < minimum):
+            minimum = margin
+            minimumIndex = index
+        if(index > 200 and maximum < margin):
+            maximum = margin
+            maximumIndex = index
+            print maximum, margin, maximumIndex
+    print maximumIndex, minimumIndex
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.line(im,(0,minimumIndex),(len(im[1,:]),minimumIndex),(255,255,0),2)
+    cv2.line(im,(0,maximumIndex),(len(im[1,:]),maximumIndex),(255,255,0),2)
+    cv2.putText(im, str(minimum),(100,minimumIndex), font, 1,(255,255,255),2)
+    cv2.putText(im, str(maximum),(100,maximumIndex), font, 1,(255,255,255),2)
+    cv2.imshow("Holes", im)
+    cv2.waitKey(20000)
 
 def drawLine(line, margin, index):
     global im
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.line(im,(0,index),(y,index),(255,255,0),2)
+    cv2.line(im,(0,index),(len(im[1,:]),index),(255,255,0),2)
     cv2.putText(im, str(margin),(100,index), font, 1,(255,255,255),2)
     cv2.imshow("Holes", im)
     cv2.waitKey(1)
